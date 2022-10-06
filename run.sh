2b33e26172fce75d9b6d369aaf6ac7ab73aceacc
John Duggan <jduggan1@vols.utk.edu>
2022-10-06 12:19:25 -0400

    Add docker pull command to run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@
 SCRIPT_DIR=$(dirname $(readlink -f "$0"))
 SSH_DIR=$HOME/.ssh
 
+docker pull seelab/substrate
 docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock --mount type=bind,src=$SCRIPT_DIR/substrate.config.yaml,dst=/root/substrate.config.yaml --mount type=bind,src=$SSH_DIR,dst=/root/.ssh seelab/substrate substrate tapestry $1
