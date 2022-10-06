15486151cc3807d2769cda1571bb9506692e0a7b
John Duggan <jduggan1@vols.utk.edu>
2022-10-06 12:41:02 -0400

    Remove echos

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ SSH_DIR=$HOME/.ssh
 docker pull seelab/substrate
 if [ "$#" -ge 2 ]
 then
-	echo "docker run ${@:1:$#-1} --rm -it -v /var/run/docker.sock:/var/run/docker.sock --mount type=bind,src=$SCRIPT_DIR/substrate.config.yaml,dst=/root/substrate.config.yaml --mount type=bind,src=$SSH_DIR,dst=/root/.ssh seelab/substrate substrate tapestry ${@: -1}"
+	docker run ${@:1:$#-1} --rm -it -v /var/run/docker.sock:/var/run/docker.sock --mount type=bind,src=$SCRIPT_DIR/substrate.config.yaml,dst=/root/substrate.config.yaml --mount type=bind,src=$SSH_DIR,dst=/root/.ssh seelab/substrate substrate tapestry ${@: -1}
 else
-	echo "docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock --mount type=bind,src=$SCRIPT_DIR/substrate.config.yaml,dst=/root/substrate.config.yaml --mount type=bind,src=$SSH_DIR,dst=/root/.ssh seelab/substrate substrate tapestry $1"
+	docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock --mount type=bind,src=$SCRIPT_DIR/substrate.config.yaml,dst=/root/substrate.config.yaml --mount type=bind,src=$SSH_DIR,dst=/root/.ssh seelab/substrate substrate tapestry $1
 fi
