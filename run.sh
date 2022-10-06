16a4a8c41db7e3c42f9914a6868d2a480408be19
John Duggan <jduggan1@vols.utk.edu>
2022-10-06 12:39:58 -0400

    Commit updated run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,9 @@ SCRIPT_DIR=$(dirname $(readlink -f "$0"))
 SSH_DIR=$HOME/.ssh
 
 docker pull seelab/substrate
-docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock --mount type=bind,src=$SCRIPT_DIR/substrate.config.yaml,dst=/root/substrate.config.yaml --mount type=bind,src=$SSH_DIR,dst=/root/.ssh seelab/substrate substrate tapestry $1
+if [ "$#" -ge 2 ]
+then
+	echo "docker run ${@:1:$#-1} --rm -it -v /var/run/docker.sock:/var/run/docker.sock --mount type=bind,src=$SCRIPT_DIR/substrate.config.yaml,dst=/root/substrate.config.yaml --mount type=bind,src=$SSH_DIR,dst=/root/.ssh seelab/substrate substrate tapestry ${@: -1}"
+else
+	echo "docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock --mount type=bind,src=$SCRIPT_DIR/substrate.config.yaml,dst=/root/substrate.config.yaml --mount type=bind,src=$SSH_DIR,dst=/root/.ssh seelab/substrate substrate tapestry $1"
+fi
