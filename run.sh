b264660d6533997731cd09d70fb67eb42c97d170
John Duggan <jduggan1@vols.utk.edu>
2022-10-25 11:10:15 -0400

    Fix bug when running without extra arguments

--- run.sh
+++ run.sh
@@ -16,7 +16,7 @@ then
 else
 	docker run --rm -it \
 		-v /var/run/docker.sock:/var/run/docker.sock \
-		--mount type=bind,src=$SCRIPT_DIR/tapestry,$SCRIPT_DIR/tapestry \
+		--mount type=bind,src=$SCRIPT_DIR/tapestry,dst=$SCRIPT_DIR/tapestry \
 		--mount type=bind,src=$SCRIPT_DIR/substrate.config.yaml,dst=/root/substrate.config.yaml \
 		--mount type=bind,src=$SSH_DIR,dst=/root/.ssh \
 		seelab/substrate:latest \
